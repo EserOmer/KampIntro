6be558422cd6daef492d51093b9a3725fe157943
EserOmer <48418294+eser2928@users.noreply.github.com>
2021-01-20 23:50:39 +0300

    20 Ocak Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Omer", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler[4] = "Ilker";
+            //Console.WriteLine(isimler[4]);
+
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Ilker");
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
